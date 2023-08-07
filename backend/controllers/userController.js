f1d619c28c0f564afe6e73c52c1fd0b729033cb4
mondirhallouli <halloulim@gmail.com>
2023-08-07 18:37:34 +0100

    added json web tokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,14 @@
 // user model for mongoose
 import User from "../models/userModel.js";
+import jwt from 'jsonwebtoken'
+// jwt secret
+const jwtSecret = process.env.JWT_SECRET
+
+// token creator function
+function createToke(_id) {
+    const token = jwt.sign({ _id }, jwtSecret, { expiresIn: "30m" })
+    return token
+}
 
 // login user
 export async function userLogin(req, res) {
@@ -15,8 +24,9 @@ export async function userSignup(req, res) {
     // run the static signup function inside a try...catch
     try {
         const user = await User.signup(email, password)
+        const token = createToke(user._id)
         // return a response
-        res.json({ email, user })
+        res.json({ email, token })
     } catch (error) {
         // return an error
         res.status(400).json({ error: error.message })
